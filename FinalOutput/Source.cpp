f9b53dbc3c42cea4b49e3e2474fe92116e71b295
DESKTOP-R03NCA5\Gandrel <ecleo552@gmail.com>
2021-10-10 16:45:10 +0800

    Add project files.

---
+++ FinalOutput/Source.cpp
@@ -0,0 +1,72 @@
+#include<iostream>;
+#include<iomanip>;
+using namespace std;
+
+//Global variables
+
+//Functions/Structures
+
+//Methods
+int main() 
+{
+	//variables
+	bool isChoosing = true;
+	int algoIndex;
+
+	while (isChoosing) 
+	{
+		cout << left
+			<< setw(10) << "[1] Sort \n"
+			<< setw(10) << "[2] Search \n"
+			<< setw(10) << "[3] Character Counting \n"
+			<< setw(10) << "[4] Exit Program \n";
+
+
+		cout << "Choose an Algorithm: ";	
+		for (;;) 
+		{
+			if (cin >> algoIndex)
+				break;
+			else {
+				cout << "Invalid Input, please try again."
+					<< endl
+					<< "Choose an Algorithm: ";
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			}
+		}
+
+		switch (algoIndex)
+		{
+		case 1:
+
+			//Quicksort Algorithm
+
+			break;
+		case 2:
+
+			//Linear Search Algorithm
+
+			break;
+
+		case 3:
+
+			//Character Counting Algorithm
+
+			break;
+		
+		case 4:
+
+			cout << "Program Ending Thank you...." << endl;
+			isChoosing = false;
+
+			break;
+
+		default:
+
+			cout << "Number out of range. Please choose again" << endl;
+
+			break;
+		}
+	}
+}
